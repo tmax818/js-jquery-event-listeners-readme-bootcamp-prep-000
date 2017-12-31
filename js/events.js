f5d59c0d0477efba9a3460a8b357584b84e5b635
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-31 18:10:18 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
-//define functions here
+function getIt() {
+  $('p').on("click", function() {
+    alert('Hey!');
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('input').on("keydown", function() {
+    
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
 
 });
